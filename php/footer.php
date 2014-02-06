8a053918aa64552fa0f888804ce3773c3c710c10
Mike Thrussell <mike.thrussell@henshaws.ac.uk>
2014-02-06 10:37:47 +0000

    new footer

--- php/footer.php
+++ php/footer.php
@@ -3,9 +3,10 @@
 <div id="footer-container">
 <hr>
 		<div class="wrapper">
-		    <a href="http://www.henshaws.org.uk/college" target="_blank" tabindex="-1"><img src="<?php echo $folder;?>img/HenshawsCollege.png" alt="Henshaws College" height="80px"></a>  
-      <span id="jisc"><img src="<?php echo $folder;?>img/JISC_Advance.png" alt="JISC Advance" height="60px"></span>
-      <img id="cc" alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-nd/3.0/88x31.png" />
+	   <a href="http://www.henshaws.org.uk/college" target="_blank" tabindex="-1"><img src="http://accesstechnology.org.uk/images/img/HenshawsCollege.png" alt="Henshaws College" height="80px"></a>  
+      <span id="jisc"><img src="http://accesstechnology.org.uk/img/jisc.png" alt="JISC Advance" height="50px"></span>
+      <img id="cc" alt="Creative Commons License" style="border-width:0" src="img/cc.png" />
+     <!-- <span id="contact"><a href="http://accesstechnology.org.uk/contact/"><img width ="40px" src="img/contact.png" alt="contact Mike Thrussell"> Contact</a></span> -->
      <!-- <span id="mt">comments and feedback: <a href="http://twitter.com/#!/mikethrussell" target="_blank">@mikethrussell</a></span> -->
 		</div>
 </div>
