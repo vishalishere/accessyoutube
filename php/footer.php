411614ba7cca8b4de60f42a8198b7e7fddfac5f1
Mike Thrussell <mike.thrussell@henshaws.ac.uk>
2014-02-06 10:54:46 +0000

    new footer

--- php/footer.php
+++ php/footer.php
@@ -3,10 +3,11 @@
 <div id="footer-container">
 <hr>
 		<div class="wrapper">
+		<span id="accesstech"><a href="http://accesstechnology.org.uk"><img src="http://accesstechnology.org.uk/img/footer.png" alt="access technology - suite of accessible web applications"</a></span>
 	   <a href="http://www.henshaws.org.uk/college" target="_blank" tabindex="-1"><img src="http://accesstechnology.org.uk/images/img/HenshawsCollege.png" alt="Henshaws College" height="80px"></a>  
       <span id="jisc"><img src="http://accesstechnology.org.uk/img/jisc.png" alt="JISC Advance" height="50px"></span>
       <img id="cc" alt="Creative Commons License" style="border-width:0" src="http://accesstechnology.org.uk/img/cc.png" />
-      <span id="accesstech"><a href="http://accesstechnology.org.uk"><img src="http://accesstechnology.org.uk/img/footer.png" alt="access technology - suite of accessible web applications"</a></span>
+
      <!-- <span id="contact"><a href="http://accesstechnology.org.uk/contact/"><img width ="40px" src="img/contact.png" alt="contact Mike Thrussell"> Contact</a></span> -->
      <!-- <span id="mt">comments and feedback: <a href="http://twitter.com/#!/mikethrussell" target="_blank">@mikethrussell</a></span> -->
 		</div>
