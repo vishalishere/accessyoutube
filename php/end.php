104979faadb5bc509a3678e4747bf29405029a34
Mike Thrussell <mike.thrussell@henshaws.ac.uk>
2014-04-03 11:55:49 +0100

    updated analytics tracking

--- php/end.php
+++ php/end.php
@@ -13,12 +13,20 @@
   <script src="<?php echo $folder;?>js/script.js"></script>
   <!-- end scripts -->
 
-  <script>
-    var _gaq=[['_setAccount','<?php echo $google_analytics; ?>'],['_trackPageview']];
-    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-    ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
-    s.parentNode.insertBefore(g,s)}(document,'script'));
-  </script>
+  <script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', '<?php echo $google_analytics; ?>']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; 
+
+ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+</script>
+
+
 </body>
 </html>
 <?php //ob_end_flush(); ?>  
