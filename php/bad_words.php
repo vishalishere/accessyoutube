dba8df425b8b3b6d7eba484d926060cc1fc14af9
Mike Thrussell <mike.thrussell@henshaws.ac.uk>
2014-09-30 12:31:22 +0100

    better esafety

---
+++ php/bad_words.php
@@ -0,0 +1,29 @@
+<?php
+
+//check for naughty words via direct url
+
+
+$query="SELECT * FROM bad_words";
+
+$result = mysql_query($query) or die (mysql_error());
+
+
+
+while ($row = mysql_fetch_assoc($result)) {
+
+
+
+$bad = $row['word'];
+
+
+
+if (preg_match("/\b$bad\b/i", $v)) {header("Location: $folder");}
+
+
+
+
+
+
+
+
+?>
