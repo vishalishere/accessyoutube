b74989773d6954b2a0cf9fffb81407e4cd4d1431
Mike Thrussell <mike.thrussell@henshaws.ac.uk>
2014-10-02 11:49:51 +0100

    HTML5

--- php/content-play.php
+++ php/content-play.php
@@ -218,64 +218,64 @@ if ($ip=="195.194.187.26") {
 
    <!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
     <iframe id="player" tabindex="-1" type="text/html" width="100%" height="100%"
-  src="https://www.youtube.com/embed/<?php echo $v;?>?autoplay=1&iv_load_policy=3&controls=0&showinfo=0&rel=0&modestbranding=1&origin=http://accessyoutube.org.uk"
+  src="https://www.youtube.com/embed/<?php echo $v;?>?enablejsapi&autoplay=1&iv_load_policy=3&controls=0&showinfo=0&rel=0&modestbranding=1&origin=http://accessyoutube.org.uk"
   frameborder="0"></iframe>
 
-    <!--<script>
-    //   // 2. This code loads the IFrame Player API code asynchronously.
-    //   var tag = document.createElement('script');
+    <script>
+      // 2. This code loads the IFrame Player API code asynchronously.
+      var tag = document.createElement('script');
 
-    //   tag.src = "https://www.youtube.com/iframe_api";
-    //   var firstScriptTag = document.getElementsByTagName('script')[0];
-    //   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-    //   // 3. This function creates an <iframe> (and YouTube player)
-    //   //    after the API code downloads.
-    //   var player;
-    //   function onYouTubeIframeAPIReady() {
-    //     player = new YT.Player('player', {
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
 
-    //       events: {
-    //         'onReady': onPlayerReady,
-    //         'onStateChange': onPlayerStateChange
-    //       }
-    //     });
-    //   }
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
 
-    //   // 4. The API will call this function when the video player is ready.
-    //   function onPlayerReady(event) {
-    //     event.target.playVideo();
-    //   }
+      // 4. The API will call this function when the video player is ready.
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
 
-    //   // 5. The API calls this function when the player's state changes.
-    //   //    The function indicates that when playing a video (state=1),
-    //   //    the player should play for six seconds and then stop.
+      // 5. The API calls this function when the player's state changes.
+      //    The function indicates that when playing a video (state=1),
+      //    the player should play for six seconds and then stop.
       
-    //   function onPlayerStateChange(event) {
-    //     if (event.data == YT.PlayerState.PLAYING) {
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING) {
 
-    //     $("#play a").click(pauseVideo);
+        $("#play a").click(pauseVideo);
           
-    //     }
+        }
 
-    //     else {
+        else {
 
-    //         $("#play a").click(playVideo);
+            $("#play a").click(playVideo);
 
-    //     }
-    //   }
+        }
+      }
       
 
-    //    function pauseVideo() {
-    //     player.pauseVideo();
-    //   }
+       function pauseVideo() {
+        player.pauseVideo();
+      }
 
 
-    //        function playVideo() {
-    //     player.playVideo();
-    //   }
+           function playVideo() {
+        player.playVideo();
+      }
 
    
-    // </script> -->
+    </script>
 
     </div>
