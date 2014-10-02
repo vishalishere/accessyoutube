17b89956e6c56c5ec45867f9ba3a81f6a5d06368
Mike Thrussell <mike.thrussell@henshaws.ac.uk>
2014-10-02 13:22:14 +0100

    HTML5

--- php/content-play.php
+++ php/content-play.php
@@ -104,7 +104,17 @@ foreach($xmlInfoVideo->children() as $title) { $videoTitle = strtoupper((string)
 <ul class="controls">  
 <?php 
 
-if(strstr($SERVER['HTTPUSERAGENT'],’iPhone’) || strstr($SERVER['HTTPUSERAGENT'],’iPod’) || strstr($SERVER['HTTPUSERAGENT'],’iPad’)){}
+//Detect special conditions devices
+$iPod    = stripos($_SERVER['HTTP_USER_AGENT'],"iPod");
+$iPhone  = stripos($_SERVER['HTTP_USER_AGENT'],"iPhone");
+$iPad    = stripos($_SERVER['HTTP_USER_AGENT'],"iPad");
+$Android = stripos($_SERVER['HTTP_USER_AGENT'],"Android");
+$webOS   = stripos($_SERVER['HTTP_USER_AGENT'],"webOS");
+
+//do something with this information
+if( $iPod || $iPhone || $ipad){
+    echo 'ios device detected!';
+}
 
 else {
 ?>
