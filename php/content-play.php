4569c1e1eeda3bd013b5f25ec75381aaf840891b
Mike Thrussell <mike.thrussell@henshaws.ac.uk>
2014-10-02 12:35:52 +0100

    HTML5

--- php/content-play.php
+++ php/content-play.php
@@ -88,8 +88,6 @@ foreach($xmlInfoVideo->children() as $title) { $videoTitle = strtoupper((string)
 			
 <div id="controlheader">		
         
-  <input type="button" id="play-button">
- <input type="button" >
   <fieldset class="search"> 
       <form method="post" name="search" action="<?php echo $folder;?>php/db.php">
       <?php focusjs('search','search')?>
