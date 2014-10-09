5cfd86107cfa536150fc305ab6f6ecd3e0e7ef84
Mike Thrussell <mike.thrussell@henshaws.ac.uk>
2014-10-09 15:33:59 +0100

    browser detection

--- browser.php
+++ browser.php
@@ -1,9 +1,11 @@
-<?PHP
+<?php
+echo $_SERVER['HTTP_USER_AGENT'] . "\n\n";
 
-foreach($_SERVER as $key_name => $key_value) {
 
-print $key_name . ” = ” . $key_value . “<br>”;
-
-}
-
+ print"<table border=0>";
+    foreach ($_SERVER as $key=>$val )
+       {
+         echo "<tr><td>".$key."</td><td>" .$val."</tr>";
+        }
+    print"</table>";
 ?>
