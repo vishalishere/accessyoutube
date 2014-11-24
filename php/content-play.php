a9b673c418e9390ed639f69199ac45e793207602
Mike Thrussell <mike.thrussell@henshaws.ac.uk>
2014-11-24 10:08:07 +0000

    tablets

--- php/content-play.php
+++ php/content-play.php
@@ -45,13 +45,248 @@ $ip =&getIP();
 
 
 
-
+//HTML5 for mobile devices
 include("Mobile_Detect.php");
 $detect = new Mobile_Detect();
 if ($detect->isMobile()) {
-    echo 'mobile browser detected';
+    
+?>
+<div id="main-container">
+  <div id="main" class="wrapper clearfix"> 
+ 
+ <?php
+$xmlInfoVideo    = simplexml_load_file("http://gdata.youtube.com/feeds/api/videos/".$v."?v=2&fields=title");
+
+foreach($xmlInfoVideo->children() as $title) { $videoTitle = strtoupper((string) $title); }
+?>
+<h3><?php echo $videoTitle;?> </h3>
+
+
+    
+<div id="controlheader">    
+ 
+  <fieldset class="search"> 
+      <form method="post" name="search" action="<?php echo $folder;?>php/db.php">
+      <?php focusjs('search','search')?>
+        <input <?php focus(search)?>  autocomplete="off" type="text" title="type here" id="<?php echo $big;?>search" class="<?php echo $big;?>search" label="type here" name="v" placeholder="type here..." />
+        <input type="hidden" name="db" value="submit">
+        <?php focusjs('btn','btn')?>
+        <input <?php focus(btn)?> type="submit" id="<?php echo $big;?>btn" class="<?php echo $big;?>btn" value="search" />  
+      </form>
+  </fieldset>
+
+
+
+
+<ul class="controls">  
+
+  <div class="control-shadow">
+    <li>
+    <?php focusjs('play','control-link')?>
+      <div id="play" class="control-link">
+        <a href="#" <?php focus(play)?> > 
+          <img src="../img/media_play_pause_resume.png" alt="Pause / Play">
+          </br>Pause / Play
+        </a>
+      </div>
+    </li>
+  </div> 
+
+  <div class="control-shadow">
+    <li>
+    <?php focusjs('volup','control-link')?>
+      <div id="volup" class="control-link">
+        <a href="#" <?php focus(volup)?> > 
+          <img src="../img/volume_up.png" alt="Volume Up">
+          </br>Volume Up
+        </a>
+      </div>
+    </li>
+  </div> 
+
+    <div class="control-shadow">
+    <li>
+    <?php focusjs('voldown','control-link')?>
+      <div id="voldown" class="control-link">
+        <a href="#" <?php focus(voldown)?> > 
+          <img src="../img/volume_down.png" alt="Volume Down">
+          </br>Volume Down
+        </a>
+      </div>
+    </li>
+  </div> 
+  
+
+  <div class="control-shadow">
+    <li>
+    <?php focusjs('repeat','control-link')?>
+      <div id="repeat" class="control-link">
+       <a href="#"  <?php focus(repeat)?> onClick="window.location.reload()">
+        <img src="../img/media_repeat.png" alt="Play Again">
+        </br>Play Again
+        </a>
+      </div>
+    </li>
+  </div> 
+
+
+<?php 
+//get next related video link
+
+relatedyt($v,$s);
+
+$s = str_replace(" ", "+", $s);    
+?>
+
+  <div class="control-shadow">
+    <li>
+    <?php focusjs('back','control-link')?>
+      <div id="back" class="control-link">
+        <a <?php focus(back)?> href="../<?php echo"$s"; ?>">
+          <img src="../img/media_previous.png" alt="Back to choices">
+          </br>Back
+        </a>
+      </div>
+    </li>
+  </div> 
+
+
+ <?php 
+
+
+//custom display for institution require close window button 
+if ($ip=="195.194.187.26") {
+ //if ($ip=="195.72.35.110") {
+?>
+
+  <div class="control-shadow">
+   <li>
+    <?php focusjs('close','control-link')?>
+      <div id="close" class="control-link">
+        <a <?php focus(close)?> href="" onclick="closeme();">
+          <img src="../img/media_close.png" alt="Close Window">
+          </br>Close Window
+        </a>
+      </div>
+    </li>
+  </div> 
+  
+  
+  <?php 
+  
+  }  
+ 
+ ?>   
+  
+  
+</ul>
+
+
+
+ </div>
+
+
+<!--   <div id=vidwrap>      
+  
+    <div id="videoDiv">Loading...</div>
+
+  </div> -->
+
+      
+    </div> <!--  #main -->
+  </div> <!-- #main-container  -->
+
+
+
+
+<div id="vidwrap" tabindex="-1">
+
+   <!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
+    <iframe id="video" tabindex="-1" type="text/html" width="100%" height="100%"
+  src="//www.youtube.com/embed/<?php echo $v;?>?enablejsapi=1&html5=1&autohide=1&autoplay=1&iv_load_policy=3&showinfo=0&rel=0&modestbranding=1&vq=large"
+  frameborder="0"></iframe>
+
+    <script>
+
+// global variable for the player
+var player;
+
+// this function gets called when API is ready to use
+function onYouTubePlayerAPIReady() {
+  // create the global player from the specific iframe (#video)
+  player = new YT.Player('video', {
+    events: {
+      // call this function when player is ready to use
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
 }
 
+
+function onPlayerReady(event) {
+
+         
+    $("#play a").click(pauseVideo);
+
+ $("#volup").click( function(){
+        if(player){
+            var currentVol = player.getVolume();
+            if((currentVol+20) <= 100){
+                player.setVolume(currentVol+20);
+                          }
+        }
+    });
+
+    $("#voldown").click( function(){
+        if(player){
+            var currentVol = player.getVolume();
+            if((currentVol-20) >= 0){
+                player.setVolume(currentVol-20);
+            }
+        }
+    });
+        
+}
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING) {
+
+        $("#play a").click(pauseVideo);
+          
+        }
+
+        else {
+
+            $("#play a").click(playVideo);
+
+        }
+      }
+      
+
+       function pauseVideo() {
+        player.pauseVideo();
+      }
+
+
+           function playVideo() {
+        player.playVideo();
+      }
+
+
+
+// Inject YouTube API script
+var tag = document.createElement('script');
+tag.src = "//www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+</script>
+
+    </div>
+<?php
+
+}
+
+//flash method for desktop machines - required to work within the Grid 2 (No way to detect Grid 2 browsing unfortunately)
 
 else {
 ?>
