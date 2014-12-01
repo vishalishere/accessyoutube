0f664dad1ba26456da62c9d6351d78b170a89fe7
Mike Thrussell <mike.thrussell@henshaws.ac.uk>
2014-12-01 14:25:16 +0000

    tablets

--- php/begin.php
+++ php/begin.php
@@ -15,7 +15,10 @@
   
   <meta name="description" content="<?php echo $site_description; ?>">
 
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" /> 
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="white" />
 
 
   <link rel="stylesheet" href="<?php echo $folder;?>css/css.php">
