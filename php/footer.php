ab42ef787321e1336cca144f312367c0f0528284
Mike Thrussell <mike.thrussell@henshaws.ac.uk>
2015-01-13 14:53:28 +0000

    .

--- php/footer.php
+++ php/footer.php
@@ -10,6 +10,7 @@
       <div class="col"><img  src="<?php echo $folder;?>img/HenshawsCollege.png" alt="henshaws college" /></div>
       <div class="col"><img  id="jisc" src="http://accesstechnology.org.uk/img/jisc.png" alt="funded by jisc" /></div>
       <div class="col"><img  alt="Creative Commons License" style="border-width:0" src="http://accesstechnology.org.uk/img/cc.png" /></div>
+      <div><p class="love">Handcoded by <a tabindex="-1" target="_blank" href="https://twitter.com/mikethrussell">Mike Thrussell</a></p>
 
              
       </div>
